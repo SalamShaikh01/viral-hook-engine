187aefd98cf5ebad67f5d395d9eeffae16622686
salam shaikh <salamshaikh4687@gmail.com>
2026-05-29 23:20:53 +0530

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,7 +47,24 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+          <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-BHM2JNR10E"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-BHM2JNR10E');
+    `}
+  </Script>
+        
+        
+        {children}</body>
     </html>
   );
 }
