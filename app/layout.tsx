ff81611a5692b4a18d9c062b4990063d8984f3a6
salam shaikh <salamshaikh4687@gmail.com>
2026-05-30 21:49:49 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,12 @@ export default function RootLayout({
       gtag('config', 'G-BHM2JNR10E');
     `}
   </Script>
+  <Script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1985370422820654"
+  crossOrigin="anonymous"
+  strategy="afterInteractive"
+/>
         
         
         {children}</body>
